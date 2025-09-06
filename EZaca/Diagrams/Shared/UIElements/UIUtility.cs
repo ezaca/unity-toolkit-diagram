b8a4f307a40878bb3f6dc850011c5ea06b9af912
EZaca <ezaca@users.noreply.github.com>
2025-09-06 17:59:31 -0300

    Add UIUtility.Gradient overloads

--- EZaca/Diagrams/Shared/UIElements/UIUtility.cs
+++ EZaca/Diagrams/Shared/UIElements/UIUtility.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using UnityEngine;
 
 namespace EZaca.UIElements
@@ -12,6 +13,26 @@ namespace EZaca.UIElements
                 mode = mode,
                 colorKeys = Array.ConvertAll(keys, key => new GradientColorKey(key.color, key.t)),
                 alphaKeys = Array.ConvertAll(keys, key => new GradientAlphaKey(key.color.a, key.t)),
+            };
+        }
+
+        public static Gradient Gradient(GradientMode mode, params Color[] colors)
+        {
+            return new Gradient()
+            {
+                mode = mode,
+                colorKeys = Enumerable.Range(0, colors.Length).Select(i => new GradientColorKey(colors[i], i / (colors.Length - 1))).ToArray(),
+                alphaKeys = Enumerable.Range(0, colors.Length).Select(i => new GradientAlphaKey(colors[i].a, i / (colors.Length - 1))).ToArray(),
+            };
+        }
+
+        public static Gradient Gradient(Color color)
+        {
+            return new Gradient()
+            {
+                mode = GradientMode.Fixed,
+                colorKeys = new[] { new GradientColorKey(color, 0f), new GradientColorKey(color, 1f) },
+                alphaKeys = new[] { new GradientAlphaKey(1f, 1f), new GradientAlphaKey(1f, 1f) },
             };
         }
     }
